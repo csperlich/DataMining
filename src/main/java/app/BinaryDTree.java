662884e0eab8a943ebb44d5ca55d41bb4abb0c0b
Cade Sperlich <csperlich@gmail.com>
2016-02-25 22:38:39 -0500

    hiring data is working for dTree?

--- src/main/java/app/BinaryDTree.java
+++ src/main/java/app/BinaryDTree.java
@@ -3,13 +3,19 @@ package app;
 import org.javatuples.Pair;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import classifier.decisiontree.DecisionTree;
 import data.attribute.AttributeInfo;
+import data.attribute.NominalAttributeInfo;
+import data.feature.DefaultFeautureStrategy;
+import data.feature.FeatureStrategy;
 import data.record.Record;
 import entropy.GiniMeasure;
 import fileio.BinaryIO;
+import fileio.HomogenousIO;
 
 public class BinaryDTree {
 	public static void main(String[] args) throws FileNotFoundException {
@@ -39,11 +45,24 @@ public class BinaryDTree {
 		System.out.println("random sampling validation error = " + dTree.validateRandomSampling(20));
 		System.out.println(dTree.getAttributeInfos());
 
-		/*
-		 * reader = new HiringDataIO(); dTree = new
-		 * DecisionTree(reader.getTrainingData("DataMining-Data/train2"), new
-		 * GiniMeasure()); dTree.buildClassifier(); dTree.printTree();
-		 */
+		List<AttributeInfo<?>> attInf = new LinkedList<>();
+		FeatureStrategy fStrat = new DefaultFeautureStrategy();
+
+		attInf.add(new NominalAttributeInfo(0, "major", Arrays.asList("cs", "other"), fStrat, false));
+		attInf.add(new NominalAttributeInfo(1, "java experience", Arrays.asList("java", "no"), fStrat, false));
+		attInf.add(new NominalAttributeInfo(2, "c/c++ experience", Arrays.asList("c/c++", "no"), fStrat, false));
+		attInf.add(new NominalAttributeInfo(3, "gpa", Arrays.asList("gpa>3", "gpa<3"), fStrat, false));
+		attInf.add(new NominalAttributeInfo(4, "large project experience", Arrays.asList("large", "small"), fStrat,
+				false));
+		attInf.add(
+				new NominalAttributeInfo(5, "years of experience", Arrays.asList("years>5", "years<5"), fStrat, false));
+
+		HomogenousIO reader2 = new HomogenousIO(attInf, fStrat);
+		List<Record> trainingData = reader2.getTrainingData("DataMining-Data/train2");
+		dTree = new DecisionTree(trainingData, attInf, new GiniMeasure());
+		dTree.buildClassifier();
+		dTree.printTree();
+
 		// dTree.printData();
 		// dTree.printTree();
 
