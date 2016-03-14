2b32b9f0df70514bb059090bdacf1a6721ddfcc7
Cade Sperlich <csperlich@gmail.com>
2016-03-14 18:33:54 -0400

    mend

--- src/main/java/app/Assignment1Part2.java
+++ src/main/java/app/Assignment1Part2.java
@@ -80,3 +80,71 @@ public class Assignment1Part2 {
 
 	}
 }
+
+/*
+ * TRACING TREE BUILD INTERNAL NODE CREATED --> internal node Node
+ * [numChildren=0, feature=Column "2" is 1, label=null, isLeaf=false,
+ * confidence=0.0, support=0.0 Records: [1 1 1 1 0 ] [1 0 0 1 1 ] [1 1 0 1 0 ]
+ * [0 1 1 1 0 ] [1 0 1 1 0 ] [1 1 0 0 0 ] [0 1 1 0 0 ] [0 1 0 0 0 ] [0 0 0 0 1 ]
+ * [0 1 0 1 1 ] [0 1 0 0 1 ] [1 0 0 0 0 ] [1 1 1 0 1 ] [1 0 0 0 1 ] [0 1 0 1 0 ]
+ * [1 0 1 1 1 ] [1 0 1 0 0 ] [0 0 1 0 1 ] [0 0 1 1 0 ] [0 1 1 1 1 ] [1 1 1 0 0 ]
+ * [1 0 0 1 0 ] [0 0 0 1 1 ] [0 0 0 1 0 ] Attribute Columns: 0 1 2 3 4
+ * 
+ * INTERNAL NODE CREATED --> internal node Node [numChildren=0, feature=Column
+ * "0" is 1, label=null, isLeaf=false, confidence=0.0, support=0.0 Records: [1 1
+ * 1 1 0 ] [0 1 1 1 0 ] [1 0 1 1 0 ] [0 1 1 0 0 ] [1 1 1 0 1 ] [1 0 1 1 1 ] [1 0
+ * 1 0 0 ] [0 0 1 0 1 ] [0 0 1 1 0 ] [0 1 1 1 1 ] [1 1 1 0 0 ] Attribute
+ * Columns: 0 1 3 4
+ * 
+ * LEAF NODE CREATED --> homogenous records Node [numChildren=0, feature=null,
+ * label=4, isLeaf=true, confidence=1.0, support=0.25 Records: [1 1 1 1 0 ] [1 0
+ * 1 1 0 ] [1 1 1 0 1 ] [1 0 1 1 1 ] [1 0 1 0 0 ] [1 1 1 0 0 ] Attribute
+ * Columns: 1 3 4
+ * 
+ * LEAF NODE CREATED --> homogenous records Node [numChildren=0, feature=null,
+ * label=1, isLeaf=true, confidence=1.0, support=0.20833333333333334 Records: [0
+ * 1 1 1 0 ] [0 1 1 0 0 ] [0 0 1 0 1 ] [0 0 1 1 0 ] [0 1 1 1 1 ] Attribute
+ * Columns: 1 3 4
+ * 
+ * INTERNAL NODE CREATED --> internal node Node [numChildren=0, feature=Column
+ * "3" is 1, label=null, isLeaf=false, confidence=0.0, support=0.0 Records: [1 0
+ * 0 1 1 ] [1 1 0 1 0 ] [1 1 0 0 0 ] [0 1 0 0 0 ] [0 0 0 0 1 ] [0 1 0 1 1 ] [0 1
+ * 0 0 1 ] [1 0 0 0 0 ] [1 0 0 0 1 ] [0 1 0 1 0 ] [1 0 0 1 0 ] [0 0 0 1 1 ] [0 0
+ * 0 1 0 ] Attribute Columns: 1 3 4 0
+ * 
+ * INTERNAL NODE CREATED --> internal node Node [numChildren=0, feature=Column
+ * "4" is 1, label=null, isLeaf=false, confidence=0.0, support=0.0 Records: [1 0
+ * 0 1 1 ] [1 1 0 1 0 ] [0 1 0 1 1 ] [0 1 0 1 0 ] [1 0 0 1 0 ] [0 0 0 1 1 ] [0 0
+ * 0 1 0 ] Attribute Columns: 1 4 0
+ * 
+ * LEAF NODE CREATED --> homogenous records Node [numChildren=0, feature=null,
+ * label=3, isLeaf=true, confidence=1.0, support=0.125 Records: [1 0 0 1 1 ] [0
+ * 1 0 1 1 ] [0 0 0 1 1 ] Attribute Columns: 1 0
+ * 
+ * LEAF NODE CREATED --> homogenous records Node [numChildren=0, feature=null,
+ * label=2, isLeaf=true, confidence=1.0, support=0.16666666666666666 Records: [1
+ * 1 0 1 0 ] [0 1 0 1 0 ] [1 0 0 1 0 ] [0 0 0 1 0 ] Attribute Columns: 1 0
+ * 
+ * LEAF NODE CREATED --> homogenous records Node [numChildren=0, feature=null,
+ * label=1, isLeaf=true, confidence=1.0, support=0.25 Records: [1 1 0 0 0 ] [0 1
+ * 0 0 0 ] [0 0 0 0 1 ] [0 1 0 0 1 ] [1 0 0 0 0 ] [1 0 0 0 1 ] Attribute
+ * Columns: 1 0 4
+ * 
+ * 
+ * PATHS TO NODES Path:Root Node:feature->Column "2" is 1 Path:Root,L
+ * Node:feature->Column "0" is 1 Path:Root,L,L Node:label->4, confidence-> 1.0,
+ * support->0.25 Path:Root,L,R Node:label->1, confidence-> 1.0,
+ * support->0.20833333333333334 Path:Root,R Node:feature->Column "3" is 1
+ * Path:Root,R,L Node:feature->Column "4" is 1 Path:Root,R,L,L Node:label->3,
+ * confidence-> 1.0, support->0.125 Path:Root,R,L,R Node:label->2, confidence->
+ * 1.0, support->0.16666666666666666 Path:Root,R,R Node:label->1, confidence->
+ * 1.0, support->0.25
+ * 
+ * CLASSIFICATION AND VALIDAITON USING GINI MEASURE Training Error: 0.0 Random
+ * Sampling Validaiton Error: 0.07726190476190475 Leave-One-Out Validation
+ * Error: 0.08333333333333333
+ * 
+ * CLASSIFICATION AND VALIDAITON USING CLASS ERROR ENTROPY Training Error: 0.0
+ * Random Sampling Validaiton Error: 0.4811666666666666 Leave-One-Out Validation
+ * Error: 0.5
+ */
