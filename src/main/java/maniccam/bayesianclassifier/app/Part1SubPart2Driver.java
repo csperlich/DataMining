f662b58277b27665f37100c21f488f90affa6dee
Cade Sperlich <csperlich@gmail.com>
2016-03-17 00:12:18 -0400

    mend

--- src/main/java/maniccam/bayesianclassifier/app/Part1SubPart2Driver.java
+++ src/main/java/maniccam/bayesianclassifier/app/Part1SubPart2Driver.java
@@ -9,14 +9,17 @@ public class Part1SubPart2Driver {
 	public static void main(String[] args) throws IOException {
 		BayesClassifier classifier = new BayesClassifier(new Part1JustNumbersDataConverter());
 
+		System.out.println("LOADING TRAINING DATA \"program2_data/part1/train1\"\n");
 		classifier.loadTrainingData("program2_data/part1/train1");
-		classifier.printRecs();
+
+		System.out.println("BUILDING MODEL\n");
 		classifier.buildModel();
 
+		System.out.println("CLASSIFYING TEST DATA \"program2_data/part1/test1\""
+				+ "\n\tand writing results to \"program2_data/part1/classified1\"\n");
 		classifier.classifyData("program2_data/part1/test1", "program2_data/part1/classified1");
 
 		System.out.println("TRAINING ERROR IS: " + classifier.trainingError());
-		System.out.println("LEAVE ONE OUT VALIDATION ERROR IS: " + classifier.validateLeaveOneOut());
-		classifier.printRecs();
+		System.out.println("LEAVE ONE OUT VALIDATION ERROR IS: " + classifier.validateLeaveOneOut() + "\n");
 	}
 }
